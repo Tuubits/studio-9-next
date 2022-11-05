217dc6782479c20270f568a6102fd94250f5a8fa
jmihc <jcornyn@gmail.com>
2022-11-04 22:17:27 -0700

    setting up grids and footer

---
+++ components/features.ts
@@ -0,0 +1,32 @@
+/* Primary image imports */
+import charms from '../public/primary/Charms.png';
+import dottieandme from '../public/primary/DottieandMe.png';
+import goldeneragaming from '../public/primary/GoldenEraGaming.png';
+/* Secondary image imports */
+import blessedisthespot from '../public/secondary/BlessedistheSpot.png';
+import firstprayertenderplant from '../public/secondary/FirstPrayerTenderPlant.png';
+import globalcrisis from '../public/secondary/GlobalCrisis.png';
+import herovsguardian from '../public/secondary/HerovsGuardian.png';
+import midnightlegion from '../public/secondary/midnightlegion.png';
+import treasuresandtraps from '../public/secondary/treasuresandtraps.png';
+import villagersandvillains from '../public/secondary/villagersandvillains.png';
+import zanjan from '../public/secondary/zanjan.png';
+import sweetneighbors from '../public/secondary/SweetNeighborsComeinAllColors.png';
+
+export const primaryFeatures = [
+    {title: 'Dottie and Me', image: dottieandme},
+    {title: 'Charms', image: charms},
+    {title: 'Golden Era Gaming', image: goldeneragaming}
+];
+
+export const secondaryFeatures = [
+    {title: 'Blessed Is the Spot', image: blessedisthespot},
+    {title: 'First Prayer Tender Plant', image: firstprayertenderplant},
+    {title: 'Sweet Neighbors Come in All Colors', image: sweetneighbors},
+    {title: 'Zanjan', image: zanjan},
+    {title: 'Villagers and Villains', image: villagersandvillains},
+    {title: 'Global Crisis', image: globalcrisis},
+    {title: 'Treasures and Traps', image: treasuresandtraps},
+    {title: 'Midnight Legion', image: midnightlegion},
+    {title: 'Hero versus Guardian: A Game of Dungeon Craft', image: herovsguardian},
+];
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { primaryFeatures, secondaryFeatures } from '../components/features';
+import { footer } from '../components/navigation';
+import { motion } from 'framer-motion';
+import headLogo from '../public/s9gamesLOGO_alpha.png';
 
 export default function Home() {
   return (
@@ -10,11 +14,92 @@ export default function Home() {
         <link rel="icon" href="/s9incLOGOsml.png" />
       </Head>
 
-      <main>
+      <main className='mx-auto max-w-7xl pt-8 px-4 sm:pt-12 sm:px-6 lg:px-8'>
+        <div>
+          <Image
+            src={headLogo}
+            className={'mx-auto'}
+            alt={'Studio 9 Inc'}
+            width={450}
+            height={450}
+          />
+        </div>
+        <p className="text-2xl py-8 px-8 sm:py-12 sm:px-12 lg:px-16 mx-auto w-full sm:w-4/5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+        <div className="grid grid-cols-1 sm:grid-cols-3 gap-2 sm:gap-4">
+        {primaryFeatures.map((i, index) => 
+          <motion.div
+            key={index}
+            className='mx-auto'
+            whileHover={{
+              position: 'relative',
+              zIndex: 1,
+            scale: 1.2,
+            transition: {
+            duration: .2
+            }}}
+            whileTap={{
+              position: 'relative',
+              zIndex: 1,
+            scale: 1.2,
+            transition: {
+            duration: .2
+            }}}
+          >
+            <Image
+              src={i.image}
+              alt={i.title}
+              width={450}
+              height={450}
+            />
+          </motion.div>
+        )}
+        </div>
+        <div className='grid grid-cols-2 gap-2 sm:grid-cols-6 sm:gap-4 pt-4'>
+        {secondaryFeatures.map((i, index) => 
+          <motion.div
+            key={index}
+            className='mx-auto'
+            whileHover={{
+              position: 'relative',
+              zIndex: 1,
+            scale: 1.2,
+            transition: {
+            duration: .2
+            }}}
+            whileTap={{
+              position: 'relative',
+              zIndex: 1,
+            scale: 1.2,
+            transition: {
+            duration: .2
+            }}}
+          >
+            <Image
+              src={i.image}
+              alt={i.title}
+              width={250}
+              height={250}
+            />
+          </motion.div>
+        )}
+        </div>
       </main>
 
-      <footer>
-      </footer>
+      <footer className="bg-white pt-8 sm:pt-12">
+      <div className="mx-auto max-w-7xl py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
+        <div className="flex justify-center space-x-6 md:order-2">
+          {footer.map((item) => (
+            <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500">
+              <span className="sr-only">{item.name}</span>
+              <item.icon className="h-6 w-6" aria-hidden="true" />
+            </a>
+          ))}
+        </div>
+        <div className="mt-8 md:order-1 md:mt-0">
+          <p className="text-center text-base text-gray-400">&copy; Studio 9 Inc. All rights reserved.</p>
+        </div>
+      </div>
+    </footer>
     </div>
   )
 }

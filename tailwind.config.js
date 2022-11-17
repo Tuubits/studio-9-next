b474ea96c0d5d329d7e65f7ebe66854178453746
jmihc <jcornyn@gmail.com>
2022-11-16 21:18:50 -0800

    update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,13 +14,6 @@ module.exports = {
     require('daisyui')
   ],
   daisyui: {
-    styled: true,
-    themes: true,
-    base: true,
-    utils: true,
-    logs: true,
-    rtl: false,
-    prefix: "",
     darkTheme: "light",
   },
 }

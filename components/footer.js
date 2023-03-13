c067814bf28b2c5968939b335491cc20be24d059
jmihc <jcornyn@gmail.com>
2023-03-13 11:42:44 -0700

    finish up footer

---
+++ components/footer.js
@@ -0,0 +1,44 @@
+import React, { useContext, useEffect, useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import Patreon from '../public/aaron.jpeg';
+
+export default function Footer() {
+    return(
+        <div data-theme={'studio9'} >
+        <footer className="footer footer-center pb-10 text-base-content rounded">
+        <div>
+          <p className='prose-xl'>Copyright © {(new Date().getFullYear())} - All right reserved by Studio 9 Inc</p>
+        </div>
+        {/* <div className="grid grid-flow-col gap-4">
+          <a className="prose-xl link link-hover">About us</a> 
+          <a className="prose-xl link link-hover">Contact</a> 
+        </div>  */}
+        <div>
+          <div className="grid grid-flow-col gap-4">
+            <Link href='https://www.twitter.com/Studio_9_Games'><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg></Link> 
+            <Link href='https://www.youtube.com/@ackreader'><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></Link>
+            <Link href='https://www.facebook.com/Studio9Inc'><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></Link>
+          </div>
+        </div>
+        <div className="grid grid-flow-col gap-4">
+            <p className='prose-xl'>Our Partners: </p>
+            <Link href='https://www.patreon.com/kreader'>
+                <Image
+                    src={Patreon}
+                    alt="Image of Aaron, linking to his Patreon"
+                    width={24}
+                    height={24}
+                />
+            </Link>
+            <Link href='https://www.rollfunkydice.com'>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                    <path d="M12 15a3 3 0 100-6 3 3 0 000 6z" />
+                    <path fillRule="evenodd" d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 010-1.113zM17.25 12a5.25 5.25 0 11-10.5 0 5.25 5.25 0 0110.5 0z" clipRule="evenodd" />
+                </svg>
+            </Link>
+        </div>
+      </footer>
+    </div>
+    )
+}

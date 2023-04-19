2b36f14eff8592e15d08a6d579847b1b3d81723d
jmihc <jcornyn@gmail.com>
2023-04-18 23:10:24 -0700

    add analytics from plausible

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,12 @@ export default function Document() {
   return (
     <Html>
       <Head>
+      <script
+            async
+            defer
+            data-domain="studio9inc.com"
+            src="https://plausible.io/js/script.js"
+          />
       </Head>
       <body>
         <Main />

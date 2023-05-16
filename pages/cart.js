c4881bf633db403a9be26aaa1d4b8483faccff85
jmihc <jcornyn@gmail.com>
2023-05-16 14:43:03 -0700

    add character limit from paypal api

--- pages/cart.js
+++ pages/cart.js
@@ -222,6 +222,7 @@ useEffect(() => {
           className="block w-full md:w-2/3 rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-xl sm:leading-6"
           onChange={(e) => setMessage(e.target.value)}
           value={message}
+          maxLength={127}
         />
       </div>
     </div>
